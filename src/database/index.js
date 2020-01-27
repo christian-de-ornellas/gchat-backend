0f53b7c00dad0eb6b893078b44368d509e67a221
Christian Possidonio <christian.ornellas@calindra.com.br>
2020-01-27 20:56:48 -0300

    Estrutura inicial ok

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * # Database
+ * Nós vamos trabalhar com o banco de dados não relacional MongoDB e vamos usar para tal o **mongoose**
+ *
+ */
+
+// Adicionamos o mongoose.
+const mongoose = require("mongoose");
+
+// Dados de conexão do MongoDB
+mongoose.connect("mongodb://localhost/gchat", { useMongoClient: true });
+
+// O mongoose vai retornar um Promise(Promessa)
+mongoose.Promise = global.Promise;
+
+//Exportar o modulo
+module.exports = mongoose;
