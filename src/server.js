efe3d8a63f91f86c24b156860c5cabfb6508a17c
Christian Possidonio <christian.ornellas@calindra.com.br>
2020-01-27 20:45:23 -0300

    Iniciando...

---
+++ src/server.js
@@ -0,0 +1,20 @@
+// O express é responsável por roterizar a API.
+const express = require("express");
+
+// O body-parser é responsável por gerenciar o app no modelo **REST**.
+const bodyParser = require("body-parser");
+
+// Instâncio aqui o express
+const app = express();
+
+// Aqui eu padronizo o body parser para sempre retornar **JSON**.
+app.use(bodyParser.json());
+
+//Responsável por entender os parametros via url.
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+    res.send({ status: "ok" });
+});
+
+app.listen(3000, () => console.log("App Listening on port 3000..."));
