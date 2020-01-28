2cc0f53dd3b3fa1001468d35410b07942551dda8
Christian Possidonio <christian.ornellas@calindra.com.br>
2020-01-28 00:01:57 -0300

    Adicionando timestamps!

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("../database");
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+
+        nickname: {
+            type: String,
+            required: true
+        }
+    },
+    { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
