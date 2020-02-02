6cee2f8a7679270b4f0cf4b5c540c7954e8e3dae
Christian Possidonio <christian.ornellas@calindra.com.br>
2020-02-02 13:53:14 -0300

    usando o cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 // O express é responsável por roterizar a API.
 const express = require("express");
-
+const cors = require("cors");
 // O body-parser é responsável por gerenciar o app no modelo **REST**.
 const bodyParser = require("body-parser");
 
@@ -9,6 +9,8 @@ const app = express();
 
 // Aqui eu padronizo o body parser para sempre retornar **JSON**.
 app.use(bodyParser.json());
+
+app.use(cors());
 
 //Responsável por entender os parametros via url.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,8 +21,8 @@ require("./controllers/UserController")(app);
 //Rota de mensagens
 require("./controllers/MessageController")(app);
 
-const server = app.listen(3000, () =>
-    console.warn("App Listening on port 3000...")
+const server = app.listen(3333, () =>
+    console.warn("App Listening on port 3333...")
 );
 
 module.exports = server;
